c8ffe858553259ba2920c63e4c35bec4d509cc84
Mika Ottjes <mottjes@c2s17d102.42wolfsburg.de>
2024-01-09 18:32:51 +0100

    first try to implement lexer

--- includes/lexer.h
+++ includes/lexer.h
@@ -6,16 +6,31 @@
 /*   By: mottjes <mottjes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 20:29:53 by mottjes           #+#    #+#             */
-/*   Updated: 2024/01/08 20:37:55 by mottjes          ###   ########.fr       */
+/*   Updated: 2024/01/09 13:50:58 by mottjes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_H
 # define LEXER_H
+/*
+
+lexer:
+lexical analysis where the input is read char by char to form tokens
+
+
+1.	word 
+2.	'-'
+'|'	
+'<'
+'>'
+'>>'
+'<<'
+*/
 
 typedef struct s_lexer
 {
 	char	*value;
+	
 	int		pos;
 	s_lexer	*next;
 }		t_lexer;
