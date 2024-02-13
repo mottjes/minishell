cb97c5c759d8c361953947119f5ca00b0974dfcb
frbeyer <frbeyer@student.42wolfsburg.de>
2024-02-13 16:13:54 +0100

    excutor updated

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mottjes <mottjes@student.42.fr>            +#+  +:+       +#+        */
+/*   By: frbeyer <frbeyer@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:55:15 by mottjes           #+#    #+#             */
-/*   Updated: 2024/02/13 14:05:57 by mottjes          ###   ########.fr       */
+/*   Updated: 2024/02/13 16:08:16 by frbeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ void	exec_built_in(t_data *shell, t_cmd *cmd)
 {
 	if (!ft_strncmp(cmd->cmd, "echo", 4))
 	{
-		if (!ft_strncmp(cmd->args[1], "-n", 2))
+		if (!ft_strncmp(cmd->args[0], "-n", 5))
 			echo(shell, 1);
 		else
 			echo(shell, 0);
@@ -69,54 +69,74 @@ void	exec_built_in(t_data *shell, t_cmd *cmd)
 	// 	export(shell);
 	// if (cmd_list->cmd == "unset")
 	// 	unset(shell);
-	else if (!ft_strncmp(cmd->cmd, "env", 3))
+	else if (!ft_strncmp(cmd->cmd, "env", 5))
 		env(shell->envp);
-	// else if (!ft_strncmp(cmd->cmd, "exit", 4))
-	// 	exit_shell(shell);
+	// if (cmd_list->cmd == "exit")
+	// 	exit(shell);
 }
 
 void	executor(t_data *shell)
 {
-	pid_t	child_pid;
-	pid_t	wait_pid;
+	pid_t	child_pid1;
+	pid_t	child_pid2; 
 	int		status;
 	int		fds[2];
 	t_cmd	*cmds;
 
+	// if (cmds->builtin == 1)
+	// 	exec_built_in(shell, cmds);
+//fds [pipe anzahl][2]
+//dementsprechend viele pipes kreieren
 	cmds = shell->cmd_list;
-	while(cmds)
+//while(cmds)
+	if (pipe(fds) == -1)
+		return ; //error // close all pipes
+	child_pid1 = fork();
+	if (child_pid1   == -1)
+		return ; //error
+	if (child_pid1 == 0)
 	{
-		// pipe(fds);
-		if (pipe(fds) == -1)
-			return ; //error
-		signals_child();
-		child_pid = fork();
-		if (child_pid == -1)
-			return ; //error
-		if (child_pid == 0)
-		{
-			dup2(fds[0], STDIN_FILENO);
-			close(fds[0]);
-			close(fds[1]);
-			if (cmds->builtin == 1)
-				exec_built_in(shell, cmds);
-			else
-				execve(cmds->path, cmds->args, shell->envp);
-			// if (shell->in_file)
-			// {
-			// 	input_from_in_file(shell);
-			// }
-			// if (shell->out_file)
-			// {
-			// 	output_in_out_file(shell);
-			// }
-			exit(0);
-		}
+		dup2(fds[1], STDOUT_FILENO);
+		//close all fds die dieser aktueller childprocess nicht braucht
 		close(fds[0]);
 		close(fds[1]);
-		cmds = cmds->next;
+		execve(cmds->path, cmds->args, shell->envp);
+		// if (shell->in_file)
+		// {
+		// 	input_from_in_file(shell);
+		// }
+		// if (shell->out_file)
+		// {
+		// 	output_in_out_file(shell);
+		// }
+		// close die fds die von diesem childporces benutzt wurden
+		 //oder return
 	}
-	wait_pid = waitpid(child_pid, &status, 0);
+	if (cmds->next)
+		cmds = cmds->next;
+	child_pid2 = fork();
+	if (child_pid2 == -1)
+		return ; //error
+	if (child_pid2 == 0)
+	{
+		dup2(fds[0], STDIN_FILENO);
+		//close all fds die dieser aktueller childprocess nicht braucht
+		close(fds[0]);
+		close(fds[1]);
+		execve(cmds->path, cmds->args, shell->envp);
+		// exit(0);
+	}
+	//danach zum nächsten childprocess
+	// cmds = cmds->next;
+	close(fds[0]);
+	close(fds[1]);
+// }
+//close evry fd that the parent is not using now
+//read stdout vom letzten childprocess 
+//print in parent ??
+//close die restlichen fds
+	waitpid(child_pid1, &status, 0); //für jeden childprocess
+	waitpid(child_pid2, &status, 0);
 }
 
 //fd[0] - read
