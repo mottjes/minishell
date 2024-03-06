35b1dde6f7983134694182e31e515e18c4936c4c
Mika Ottjes <mottjes@c2r12s4.42wolfsburg.de>
2024-03-06 15:21:02 +0100

    save

--- src/input/input.c
+++ src/input/input.c
@@ -6,7 +6,7 @@
 /*   By: mottjes <mottjes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 13:45:56 by mottjes           #+#    #+#             */
-/*   Updated: 2024/03/06 15:14:46 by mottjes          ###   ########.fr       */
+/*   Updated: 2024/03/06 15:20:15 by mottjes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	input(t_data *shell)
 		shell->exit_status = 0;
 		// free_all(shell);
 		ft_putstr_fd("exit\n", 1);
-		exit(0);
+		exit(shell->exit_status);
 	}
 	add_history(shell->input);
 }
