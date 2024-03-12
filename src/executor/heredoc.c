e96a85f62c022a7f9d9168d72137a04ec408c195
Frederik Beyer <frbeyer@c2r12s1.42wolfsburg.de>
2024-03-12 14:01:07 +0100

    heredoc ready

--- src/executor/heredoc.c
+++ src/executor/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: frbeyer <frbeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:55:15 by mottjes           #+#    #+#             */
-/*   Updated: 2024/03/11 17:53:10 by frbeyer          ###   ########.fr       */
+/*   Updated: 2024/03/12 13:59:21 by frbeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,16 +41,28 @@ int	has_heredoc(t_data *shell)
 	return (0);
 }
 
+static void	read_line(t_token *token, int fd)
+{
+	char	*line;
+
+	while (1)
+	{
+		line = readline("> ");
+		if (!ft_strncmp(line, token->next->str,
+				ft_strlen(token->next->str)))
+			return ;
+		ft_putendl_fd(line, fd);
+		free(line);
+	}
+}
+
 void	capture_heredoc(t_data *shell)
 {
 	t_token	*token;
-	char	*line;
 	int		fd[2];
-	int		i;
 
-	i = 0;
 	token = shell->token_list;
-	while (i < shell->count_heredoc)
+	while (shell->count_heredoc > 0)
 	{
 		if (pipe(fd) == -1)
 			pipe_fail(shell);
@@ -59,15 +71,7 @@ void	capture_heredoc(t_data *shell)
 		{
 			if (token->type == HERE_DOC)
 			{
-				while (1)
-				{
-					line = readline("> ");
-					if (!ft_strncmp(line, token->next->str,
-							ft_strlen(token->next->str)))
-						break ;
-					ft_putendl_fd(line, fd[1]); // need to overwrite it
-					free(line);
-				}
+				read_line(token, fd[1]);
 				token = token->next;
 				break ;
 			}
@@ -75,7 +79,8 @@ void	capture_heredoc(t_data *shell)
 				token = token->next;
 		}
 		close (fd[1]);
-		i++;
+		shell->count_heredoc--;
 	}
 	shell->count_heredoc = 0;
 }
+
